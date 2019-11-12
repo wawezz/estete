2d661786d4182dc9643f261d996fab5ba4aa162d
wawezz <wawezz@Tigrans-MacBook-Pro.local>
2019-11-13 01:12:35 +0400

    menu registration

--- functions.php
+++ functions.php
@@ -1,17 +1,1 @@
 <?php
-
-function add_menu_support() {
-    register_nav_menus([
-		'header_menu' => 'Header menu',
-        'footer_menu' => 'Footer menu',
-        'social_menu' => 'Social menu'
-	]);
-}
-
-function after_setup() {
-    add_menu_support();
-}
-
-add_action('after_setup_theme', 'after_setup');
-
-?>
