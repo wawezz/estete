23b81a36234a82ebe1c928f595500e68276be1ea
wawezz <wawezz@Tigrans-MacBook-Pro.local>
2019-11-13 01:19:38 +0400

    menu registration

--- footer.php
+++ footer.php
@@ -1,4 +1,22 @@
 <footer class="site-footer">
+    <?php wp_nav_menu( [
+        'theme_location'  => '',
+        'menu'            => 'footer_menu', 
+        'container'       => 'div', 
+        'container_class' => '', 
+        'container_id'    => '',
+        'menu_class'      => 'menu', 
+        'menu_id'         => '',
+        'echo'            => true,
+        'fallback_cb'     => 'wp_page_menu',
+        'before'          => '',
+        'after'           => '',
+        'link_before'     => '',
+        'link_after'      => '',
+        'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+        'depth'           => 0,
+        'walker'          => '',
+    ] ) ?>
     <p><?php bloginfo('name')?></p>
 </footer>
  
--- header.php
+++ header.php
@@ -19,6 +19,24 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <?php wp_nav_menu([
+                'theme_location'  => '',
+                'menu'            => 'header_menu', 
+                'container'       => 'div', 
+                'container_class' => '', 
+                'container_id'    => '',
+                'menu_class'      => 'menu', 
+                'menu_id'         => '',
+                'echo'            => true,
+                'fallback_cb'     => 'wp_page_menu',
+                'before'          => '',
+                'after'           => '',
+                'link_before'     => '',
+                'link_after'      => '',
+                'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                'depth'           => 0,
+                'walker'          => '',
+            ]) ?>
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
@@ -33,6 +51,24 @@
                     <a class="nav-link" href="#">Opinions</a>
                 </li>
             </ul>
+            <?php wp_nav_menu([
+                'theme_location'  => '',
+                'menu'            => 'social_menu', 
+                'container'       => 'div', 
+                'container_class' => '', 
+                'container_id'    => '',
+                'menu_class'      => 'menu', 
+                'menu_id'         => '',
+                'echo'            => true,
+                'fallback_cb'     => 'wp_page_menu',
+                'before'          => '',
+                'after'           => '',
+                'link_before'     => '',
+                'link_after'      => '',
+                'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                'depth'           => 0,
+                'walker'          => '',
+            ]) ?>
             <ul class="navbar-nav nav-flex-icons">
                 <li class="nav-item">
                     <a href="https://www.facebook.com/" class="nav-link"><i class="fab fa-facebook-f"></i></a>
