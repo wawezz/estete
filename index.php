d0d51580929b829de81c0a58ba56667c00f3d307
wawezz <wawezz@Tigrans-MacBook-Pro.local>
2019-11-13 01:18:37 +0400

    menu registration

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 get_header();
  
 if ( have_posts() ) :
-
+	while ( have_posts() ) : the_post(); ?>
+ 
+        <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+		<?php the_content() ?>
+	
+	<?php endwhile;
 else :
 	echo '<p>There are no posts!</p>';
 endif;
