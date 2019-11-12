dbaa19a12a3758036abd7c57dbb0d5c33efffd98
wawezz <wawezz@Tigrans-MacBook-Pro.local>
2019-11-13 02:22:27 +0400

    custom post type add

--- functions.php
+++ functions.php
@@ -15,20 +15,47 @@ function after_setup() {
     add_menu_support();
 }
 
+function property_custom_post_type() {
+	$labels = array(
+		'name'                => __('Properties', 'estete-theme'),
+		'singular_name'       => __('Property', 'estete-theme'),
+		'menu_name'           => __( 'Properties', 'estete-theme'),
+		'parent_item_colon'   => __( 'Parent Property', 'estete-theme'),
+		'all_items'           => __( 'All Properties', 'estete-theme'),
+		'view_item'           => __( 'View Property', 'estete-theme'),
+		'add_new_item'        => __( 'Add New Property', 'estete-theme'),
+		'add_new'             => __( 'Add New', 'estete-theme'),
+		'edit_item'           => __( 'Edit Property', 'estete-theme'),
+		'update_item'         => __( 'Update Property', 'estete-theme'),
+		'search_items'        => __( 'Search Property', 'estete-theme'),
+		'not_found'           => __( 'Not Found', 'estete-theme'),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'estete-theme')
+	);
+	$args = array(
+		'label'               => __( 'properties', 'estete-theme'),
+		'description'         => __( 'Property records', 'estete-theme'),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'author', 'revisions', 'custom-fields'),
+		'public'              => true,
+		'hierarchical'        => false,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'has_archive'         => true,
+		'can_export'          => true,
+		'exclude_from_search' => false,
+	        'yarpp_support'       => true,
+		'taxonomies' 	      => array('city'),
+		'publicly_queryable'  => true,
+		'capability_type'     => 'post'
+);
+	register_post_type( 'property', $args );
+}
+
 function add_custom_post_types()
 {
-    register_post_type('property',
-        array(
-            'labels'      => array(
-                'name'          => __('Properties', 'estete-theme'),
-                'singular_name' => __('Property', 'estete-theme'),
-            ),
-            'public'      => true,
-            'has_archive' => true,
-            'rewrite'     => array( 'slug' => 'properties' ),
-            'taxonomies'  => array('city'),
-        )
-    );
+    property_custom_post_type();
 }
 
 add_action('init', 'add_custom_post_types');
