ab9f09184e2c70c883234e7273fed1c0bd096111
wawezz <wawezz@Tigrans-MacBook-Pro.local>
2019-11-13 01:45:12 +0400

    custom post type add

--- functions.php
+++ functions.php
@@ -15,5 +15,22 @@ function after_setup() {
     add_menu_support();
 }
 
+function add_custom_post_types()
+{
+    register_post_type('property',
+        array(
+            'labels'      => array(
+                'name'          => __('Properties', 'estete-theme'),
+                'singular_name' => __('Property', 'estete-theme'),
+            ),
+            'public'      => true,
+            'has_archive' => true,
+            'rewrite'     => array( 'slug' => 'properties' ),
+            'taxonomies'  => array('city'),
+        )
+    );
+}
+
+add_action('init', 'add_custom_post_types');
 add_action('wp_enqueue_scripts', 'custom_theme_assets');
 add_action('after_setup_theme', 'after_setup');
