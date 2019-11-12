f118ea609b13ebceaa1c7c973228de5d3638d367
wawezz <wawezz@Tigrans-MacBook-Pro.local>
2019-11-13 01:08:07 +0400

    menu registration

--- header.php
+++ header.php
@@ -51,24 +51,6 @@
                     <a class="nav-link" href="#">Opinions</a>
                 </li>
             </ul>
-            <?php wp_nav_menu([
-                'theme_location'  => '',
-                'menu'            => 'social_menu', 
-                'container'       => 'div', 
-                'container_class' => '', 
-                'container_id'    => '',
-                'menu_class'      => 'menu', 
-                'menu_id'         => '',
-                'echo'            => true,
-                'fallback_cb'     => 'wp_page_menu',
-                'before'          => '',
-                'after'           => '',
-                'link_before'     => '',
-                'link_after'      => '',
-                'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                'depth'           => 0,
-                'walker'          => '',
-            ]) ?>
             <ul class="navbar-nav nav-flex-icons">
                 <li class="nav-item">
                     <a href="https://www.facebook.com/" class="nav-link"><i class="fab fa-facebook-f"></i></a>
