e05728f9792cbbea059fe7840eb1bc31ddd9d989
wawezz <wawezz@Tigrans-MacBook-Pro.local>
2019-11-13 01:09:42 +0400

    menu registration

--- functions.php
+++ functions.php
@@ -1,7 +1,4 @@
 <?php
-function custom_theme_assets() {
-	wp_enqueue_style('style', get_stylesheet_uri());
-}
 
 function add_menu_support() {
     register_nav_menus([
@@ -15,5 +12,4 @@ function after_setup() {
     add_menu_support();
 }
 
-add_action('wp_enqueue_scripts', 'custom_theme_assets');
 add_action('after_setup_theme', 'after_setup');
