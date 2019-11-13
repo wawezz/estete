7a2abbfcf17e87b7934689b67aa85cd36216fc41
wawezz <wawezz@Tigrans-MacBook-Pro.local>
2019-11-13 18:52:57 +0400

    thumb support

--- single.php
+++ single.php
@@ -1,12 +1,14 @@
 <?php get_header()?>
 <div class="container p-3">
     <?php if (have_posts()) :
-        while (have_posts()) : the_post(); 
-                if (has_post_thumbnail()) :
-                    the_post_thumbnail('large');
-                else :
-                    echo '<img src="'.get_template_directory_uri().'/images/no-image.jpg" class="wp-post-image" />';
-                endif; ?>
+        while (have_posts()) : the_post(); ?>
+                <div class="flex mb-6">
+                    <?php if (has_post_thumbnail()) :
+                        the_post_thumbnail('large');
+                    else :
+                        echo '<img src="'.get_template_directory_uri().'/images/no-image.jpg" class="wp-post-image" />';
+                    endif; ?>
+                </div>
             <h2 class="text-dark mb-4"><a href="<?php the_permalink()?>"><?php the_title()?></a></h2>
             <?php the_content()?>
         
