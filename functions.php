dd5f3c546325983b4210393ab26d813a0c9800af
wawezz <wawezz@Tigrans-MacBook-Pro.local>
2019-11-13 17:42:07 +0400

    single

--- functions.php
+++ functions.php
@@ -88,4 +88,4 @@ function custom_excerpt_length( $length = 20 ) {
 add_action('init', 'add_custom_post_types_and_taxonomies');
 add_action('wp_enqueue_scripts', 'custom_theme_assets');
 add_action('after_setup_theme', 'after_setup');
-add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+add_filter('excerpt_length', 'custom_excerpt_length');
