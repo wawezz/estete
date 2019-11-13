52d24ef85c6f30c290a2050daedb947b181835ae
wawezz <wawezz@Tigrans-MacBook-Pro.local>
2019-11-13 17:32:20 +0400

    index fixes

---
+++ post.php
@@ -0,0 +1,12 @@
+<?php get_header() ?>
+<div class="container p-3">
+    <?php if ( have_posts() ) :
+        while ( have_posts() ) : the_post(); ?>
+    
+            <h2 class="text-dark mb-4"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+            <?php the_content() ?>
+        
+        <?php endwhile;
+    endif; ?>
+</div>
+<?php get_footer(); ?>
