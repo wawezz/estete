8877e402f82c83aa4888d2f98ae33afd8699a2e1
wawezz <wawezz@Tigrans-MacBook-Pro.local>
2019-11-13 16:54:09 +0400

    menu fix

--- page.php
+++ page.php
@@ -1,6 +1,6 @@
 <?php get_header() ?>
 <div class="container">
-    <? if ( have_posts() ) :
+    <?php if ( have_posts() ) :
         while ( have_posts() ) : the_post(); ?>
     
             <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
