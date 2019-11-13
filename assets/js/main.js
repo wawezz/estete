842750e8701b45f0f8237c8acee4d1afc8ab855e
wawezz <wawezz@Tigrans-MacBook-Pro.local>
2019-11-14 01:31:49 +0400

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,37 @@
-(function ($) {
+(function($) {
+  $(document).on("click", ".filter-icon", function() {
+    $(".filter").toggleClass("active");
+  });
 
-    $(document).on("click", ".filter-icon", function () {
-        $(".filter").toggleClass("active");
-    });
-
-    $(document).on("change", ".filter-city", function () {
-        getProperties();
-    });
-
-    function getProperties() {
-        var filter = $(".filter-city");
-
-        filter.prop('disabled', 'disabled');
-        $(".loading-spin").show();
-        var cities = filter.val();
-
-        $.ajax({
-            type: "POST",
-            dataType: "html",
-            url: the_ajax_script.ajaxurl,
-            data: {
-                cities: cities,
-                action: 'get_proporties_ajax'
-            },
-            success: function (data) {
-                $(".properties-list").html(data);
-                filter.prop('disabled', false);
-                $(".loading-spin").hide();
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                $loader.html(jqXHR + " :: " + textStatus + " :: " + errorThrown);
-                filter.prop('disabled', false);
-                $(".loading-spin").hide();
-            }
-        });
-    }
-
+  $(document).on("change", ".filter-city", function() {
     getProperties();
+  });
+
+  function getProperties() {
+    var filter = $(".filter-city");
+
+    filter.prop("disabled", "disabled");
+    $(".loading-spin").show();
+    var cities = filter.val();
+
+    $.ajax({
+      type: "POST",
+      dataType: "html",
+      url: the_ajax_script.ajaxurl,
+      data: {
+        cities: cities,
+        action: "get_proporties_ajax"
+      },
+      success: function(data) {
+        $(".properties-list").html(data);
+        filter.prop("disabled", false);
+        $(".loading-spin").hide();
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        $loader.html(jqXHR + " :: " + textStatus + " :: " + errorThrown);
+        filter.prop("disabled", false);
+        $(".loading-spin").hide();
+      }
+    });
+  }
 })(jQuery);
