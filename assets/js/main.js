08a05ab80d23fb34867de91a79f6437c5c4c37e1
wawezz <wawezz@Tigrans-MacBook-Pro.local>
2019-11-14 02:03:06 +0400

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,34 +6,32 @@
   $(document).on("change", ".filter-city", function() {
     getProperties();
   });
-
-  function getProperties() {
-    var filter = $(".filter-city");
-
-    filter.prop("disabled", "disabled");
-    $(".loading-spin").show();
-    var cities = filter.val();
-
-    $.ajax({
-      type: "POST",
-      dataType: "html",
-      url: the_ajax_script.ajaxurl,
-      data: {
-        cities: cities,
-        action: "get_proporties_ajax"
-      },
-      success: function(data) {
-        $(".properties-list").html(data);
-        filter.prop("disabled", false);
-        $(".loading-spin").hide();
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        $loader.html(jqXHR + " :: " + textStatus + " :: " + errorThrown);
-        filter.prop("disabled", false);
-        $(".loading-spin").hide();
-      }
-    });
-  }
-
-  add_action("wp_head", "getProperties");
 })(jQuery);
+
+function getProperties() {
+  var filter = $(".filter-city");
+
+  filter.prop("disabled", "disabled");
+  $(".loading-spin").show();
+  var cities = filter.val();
+
+  $.ajax({
+    type: "POST",
+    dataType: "html",
+    url: the_ajax_script.ajaxurl,
+    data: {
+      cities: cities,
+      action: "get_proporties_ajax"
+    },
+    success: function(data) {
+      $(".properties-list").html(data);
+      filter.prop("disabled", false);
+      $(".loading-spin").hide();
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      $loader.html(jqXHR + " :: " + textStatus + " :: " + errorThrown);
+      filter.prop("disabled", false);
+      $(".loading-spin").hide();
+    }
+  });
+}
