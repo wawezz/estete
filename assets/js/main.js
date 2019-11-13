9bd4d5eae2eef9c085c8c937e0fb1668c358df37
wawezz <wawezz@Tigrans-MacBook-Pro.local>
2019-11-13 21:19:18 +0400

    filter add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,31 @@
 (function ($) {
-    var filter = [];
 
     $(document).on("click", ".filter-icon", function () {
         $(".filter").toggleClass("active");
     });
 
     $(document).on("change", ".filter-city", function () {
-        console.log($(this).val());
+        $(this).prop('disabled', 'disabled');
+
+        var cities = $(this).val();
+
+        $.ajax({
+            type: "POST",
+            dataType: "html",
+            url: the_ajax_script.ajaxurl,
+            data: {
+                cities: cities,
+                action: 'get_proporties_ajax'
+            },
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $loader.html(jqXHR + " :: " + textStatus + " :: " + errorThrown);
+            }
+
+        });
+
+        $(this).prop('disabled', false);
     });
 })(jQuery);
