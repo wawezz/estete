93d1e01bbda5ed745a9214a50eeeca8b9d40737a
wawezz <wawezz@Tigrans-MacBook-Pro.local>
2019-11-13 16:53:34 +0400

    menu fix

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
+<?php get_header() ?>
 <div class="container">
-    <?php
-    get_header();
-    
-    if ( have_posts() ) :
+    <? if ( have_posts() ) :
         while ( have_posts() ) : the_post(); ?>
     
             <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
@@ -11,8 +9,7 @@
         <?php endwhile;
     else :
         echo '<p>There are no posts!</p>';
-    endif;
-    
-    get_footer();
-    ?>
+    endif; ?>
+</div>
+<?php get_footer(); ?>
 </div>
