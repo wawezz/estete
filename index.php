5290016292bc5dcd7858acd8fd0f9bc094d87e6e
wawezz <wawezz@Tigrans-MacBook-Pro.local>
2019-11-13 19:48:54 +0400

    filter add

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <h2 class="display-4"><?php _e('A Warm Welcome!', 'estete-theme')?></h2>
         <p class="lead"><?php _e('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.', 'estete-theme')?></p>
     </header>
-    <div class="row filter text-right">
+    <div class="row filter justify-content-end">
         <i class="fas fa-filter filter-icon"></i>
         <div class="col-12 filter-body">
             <select>
