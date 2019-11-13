b994a2c717483e18972c35fc0e7fcdb49c1fb0bf
wawezz <wawezz@Tigrans-MacBook-Pro.local>
2019-11-13 17:23:31 +0400

    index fixes

--- index.php
+++ index.php
@@ -1,21 +1,34 @@
 <?php get_header() ?>
 <div class="container p-3">
-    <?php 
-    $args = array(
-        'post_type'=> 'properties',
-    //    'areas'    => 'painting',
-        'order'    => 'ASC'
-        );              
+    <div class="row text-center">
+        <?php 
+        $args = array(
+            'post_type'=> 'properties',
+        //    'areas'    => 'painting',
+            'order'    => 'ASC'
+            );              
 
-    $the_query = new WP_Query( $args );
-    if($the_query->have_posts() ) : 
-        while ( $the_query->have_posts() ) : 
-            $the_query->the_post(); 
-            the_title();
-        endwhile;
-    else :
-        echo '<p>There are no posts!</p>';
-    endif;
-    ?>
+        $the_query = new WP_Query( $args );
+        if($the_query->have_posts() ) : 
+            while ( $the_query->have_posts() ) : 
+                $the_query->the_post(); ?>
+                <div class="col-lg-3 col-md-6 mb-4">
+                    <div class="card h-100">
+                        <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+                        <div class="card-body">
+                            <h4 class="card-title"><?php the_title()?></h4>
+                            <p class="card-text"><?php the_exerpt()?></p>
+                        </div>
+                        <div class="card-footer">
+                            <a href="#" class="btn btn-primary">Find Out More!</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endwhile;
+        else :
+            echo '<div class="col-12">'.__('There are no posts!', 'estete-theme').'</div>';
+        endif;
+        ?>
+    </div>
 </div>
 <?php get_footer(); ?>
